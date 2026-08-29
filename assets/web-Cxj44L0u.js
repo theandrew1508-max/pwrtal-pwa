306100fbc4a6d248d27cc3cbb1e4370c54f04142
Andrew <theandrew1508@googlemail.com>
2026-08-29 14:17:31 +0100

    Fix floating-point display bug in progress bar current value

--- assets/web-Cxj44L0u.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-53q3uPUZ.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
---
+++ assets/web-DHwIBHZW.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-DIWzg8vR.js";var t=class extends e{async show(e){}async hide(e){}};export{t as SplashScreenWeb};
